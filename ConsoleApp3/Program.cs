ad3bc55eb6d5f8b4c15ee9538e1ff231494e41f9
anzori <anzoareshidze@gmail.com>
2023-06-14 16:14:33 +0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,99 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+/*
+Console.Write("Enter Rectangle length: ");
+double length = double.Parse(Console.ReadLine());
+
+Console.Write("Enter Rectangle width: ");
+double width = double.Parse(Console.ReadLine());
+
+double area = CalculateArea(length, width);
+Console.WriteLine("The area of the rectangle is: " + area);
+
+static double CalculateArea(double length,double width)
+{
+	double area = length * width;
+	return area;	
+}
+
+/////////////////////////
+
+
+static int SpaceCheck(string text)
+{
+
+	int CountSpace = 0;
+
+	for (int i = 0; i < text.Length; i++)
+	{
+		if (text[i] == ' ')
+		{
+			CountSpace++;
+		}
+	}
+
+	return CountSpace;
+}
+
+Console.Write("Enter rext: ");
+string text = Console.ReadLine();
+
+Console.WriteLine(SpaceCheck(text));
+
+/////////////////////////
+
+
+int[] numbers = { 12, 5, 64, 34, 37, 65 };
+
+int maxNumber = numbers.Max();
+Console.WriteLine($"max number is: {maxNumber}");
+
+//////////////////////////
+
+int number;
+int sum = 0;
+Console.Write("Enter number: ");
+
+number = int.Parse(Console.ReadLine());
+
+string digits = Convert.ToString(number);
+
+for (int i = 0; i < digits.Length; i++)
+{
+	sum = sum + Convert.ToInt32(digits[i].ToString()); ;
+}
+Console.WriteLine($"Sum of the digits is: {sum}");
+
+
+/////////////////////////////////////////////
+
+var random = new Random();
+
+int grade = random.Next(101);
+
+if (grade <= 50)
+{
+	Console.WriteLine("U got: F");
+}
+else if (grade <= 60 && grade >= 51)
+{
+    Console.WriteLine("U got: E");
+}
+else if (grade <= 70 && grade >= 61)
+{
+	Console.WriteLine("U got: D");
+}
+else if (grade <= 80 && grade >= 71)
+{ 
+	Console.WriteLine("U got: C");
+}
+else if (grade <= 90 && grade >= 81)
+{
+	Console.WriteLine("U got: B");
+}
+else if (grade <= 100 && grade >= 91)
+{
+	Console.WriteLine("U got: E");
+}
+*/
